ddfd80fa252eab56bff79b6372785877630d186a
Jonathan Berger <john@3kd.be>
2017-05-25 01:25:51 +0200

    bla bla bla

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -33,11 +33,38 @@ class FacebookController extends Controller {
 		 *  - retrieve data from payload (user_id etc.)
 		 *  - request to FB Graph API with user_id
 		 */
-		DB::insert('insert into 
+
+		// {"changes":[{"field":"feed","value": {"item":"like","verb":"add","sender_id":"739017722955825"}}],"id":"1936253853323830","time":1495480999}
+
+		if(!$json)
+			return null;
+
+		foreach($json['changes'] as $change) {
+			$userData = $this->_getFbUserData($change['sender_id']);
+
+			if(!$userData)
+				return null;
+
+			if($change['value']['item'] == 'like' && $change['value']['verb'] == 'add') {
+				$actionText = 'aime la page';
+				$action = 'page_like';
+			} else {
+				$actionText = $change['value']['item'] . ' / ' . $change['value']['verb'];
+				$action = 'unknown';
+			}
+
+
+			DB::insert('insert into 
 					users_data (`name`,`action`,action_text,profile_picture_url,`timestamp`) 
 					values (?,?,?,?,?)',
-					[$json->name, $json->action, $json->action_text, $json->profile_picture_url,$json->timestamp]
-		);
+				[$userData->name, $action, $actionText, $userData->profile_picture_url,$json->time]
+			);
+		}
+
 		return new JsonResponse(json_decode($request->getContent(), true));
+	}
+
+	private function _getFbUserData($userId) {
+
 	}
 }
