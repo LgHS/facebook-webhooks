08c82f3429086c8eb96521637a88ebc23fed72fc
Greg Berger <greg@3kd.be>
2017-05-25 02:05:04 +0200

    added thermal controller actual data

--- app/Http/Controllers/ThermalController.php
+++ app/Http/Controllers/ThermalController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 
@@ -10,35 +9,20 @@ use Illuminate\Support\Facades\DB;
 
 class ThermalController extends Controller {
 	public function index(Request $request) {
-		return new JsonResponse([
-			[
-				'sender_id' => '10156160031998989',
-				'name' => 'Jonathan Berger',
-				'action' => 'page_like',
-				'timestamp' => '1495480999',
-				'profile_public_url' => 'https://scontent.xx.fbcdn.net/v/t1.0-1/p480x480/1926855_10152679111558989_489741242_n.jpg?oh=60eedad63dc03b6824b398c078d55e4e&oe=59A80419'
-			],
-			[
-				'sender_id' => '10156160031998989',
-				'name' => 'Dominique Nimous',
-				'action' => 'post_comment',
-				'timestamp' => '1495480999',
-				'profile_public_url' => 'https://scontent.xx.fbcdn.net/v/t1.0-1/p480x480/1926855_10152679111558989_489741242_n.jpg?oh=60eedad63dc03b6824b398c078d55e4e&oe=59A80419'
-			],
-			[
-				'sender_id' => '10156160031998989',
-				'name' => 'Jonathan Berger',
-				'action' => 'post_share',
-				'timestamp' => '1495480999',
-				'profile_public_url' => 'https://scontent.xx.fbcdn.net/v/t1.0-1/p480x480/1926855_10152679111558989_489741242_n.jpg?oh=60eedad63dc03b6824b398c078d55e4e&oe=59A80419'
-			],
-			[
-				'sender_id' => '10156160031998989',
-				'name' => 'Jerome K',
-				'action' => 'page_like',
-				'timestamp' => '1495480999',
-				'profile_public_url' => 'https://scontent.xx.fbcdn.net/v/t1.0-1/p480x480/1926855_10152679111558989_489741242_n.jpg?oh=60eedad63dc03b6824b398c078d55e4e&oe=59A80419'
-			],
-		]);
+
+		$data  = DB::select('SELECT * FROM users_data');
+		$json = [];
+		foreach ($data as $d){
+			$json[] =
+				[
+					'name' => $d->name,
+					'action' => $d->action,
+					'timestamp' => $d->timestamp,
+					'profile_public_url' => $d->profile_picture_url
+				];
+			DB::delete('DELETE from users_data WHERE id = ?', [$d->id]);
+		}
+
+		return new JsonResponse($json);
 	}
 }
